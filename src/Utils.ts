71a034b3ad6bc91cf177c7f1cc4813b348ef3d98
Mike Alicea <malicea@squarespace.com>
2024-10-21 14:27:06 -0400

    seconds to filename

--- src/Utils.ts
+++ src/Utils.ts
@@ -13,7 +13,8 @@ export function generateFileName(prefix: string, extension: string): string {
   const day = String(now.getDate()).padStart(2, '0');
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
-  return `${prefix}-${year}${month}${day}-${hours}.${minutes}.${extension}`;
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  return `${prefix}-${year}${month}${day}-${hours}.${minutes}.${seconds}.${extension}`;
 }
 export async function saveFile(
   app: App,
